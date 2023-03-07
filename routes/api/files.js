e1eda3ed7715faff8643bf966643e4f4333fe9ed
YurGoV <yurgov@gmail.com>
2023-03-07 16:54:58 +0200

    step-1/added-SendGrid-key-to-.env

--- routes/api/files.js
+++ routes/api/files.js
@@ -5,7 +5,8 @@ require('dotenv').config();
 
 const FILE_DIR_ENV = process.env.FILE_DIR_ENV;
 const FILE_DIR =  path.resolve(FILE_DIR_ENV);
-
+// todo: two the same in .env
+// todo: separate path/path in .env??
 
 router.use('/', express.static(FILE_DIR));
 
